8f765788f05bed08bb4476e47cf85ca64174f3ac
Haneen111Haleem <haneenhaleem111@gmail.com>
2023-07-14 10:43:07 -0700

    five

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase, then in uppercase.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+
+	int c;
+
+	for (c =0; c < 10; c++)
+	{
+		putchar(c);
+	}
+	
+	putchar('\n');
+	return (0);
+
+}
