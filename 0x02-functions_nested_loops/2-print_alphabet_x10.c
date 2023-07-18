c390f6dc9e25d2d080bb7b8ed7ce8e70c55c3498
Haneen111Haleem <haneenhaleem111@gmail.com>
2023-07-18 07:39:58 -0700

    two

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+	int i=0;
+
+	while(i < 10)
+	{
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+
+		}	
+		_putchar('\n');
+		i++;
+	}
+
+}
