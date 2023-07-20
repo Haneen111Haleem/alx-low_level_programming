4710431a479f0542b49fdc2be2895dac5341c008
Haneen111Haleem <haneenhaleem111@gmail.com>
2023-07-20 09:21:07 -0700

    five

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,12 @@ for (number1 = 0; number1 < 10; number1++)
 for (number2 = 0; number2 <= 14; number2++)
 
 {
-	_putchar(number2);
+if (number2 > 9)
+{
+_putchar((number2 / 10) + '0');
+}
+_putchar((number2 % 10) + '0');
 }
 _putchar(10);
+}
 }
